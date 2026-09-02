7968612064564325a0e0025909630b142b0d61f1
Gangadhar Akkammagari <gangadharroy9999@gmail.com>
2026-09-02 11:31:54 +0530

    Initial commit - Movie Picture Pipeline

---
+++ starter/backend/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+
+try:
+    from .movies import movies_api
+except ImportError:  # support running as a direct script
+    from movies import movies_api
+
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(movies_api)
+
+# Start app
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        port=int(os.getenv("FLASK_RUN_PORT", 5000)),
+    )
